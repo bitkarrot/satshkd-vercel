b4cb943a914bfc85a56501e7bf02ad4bb851f4a6
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-10-07 06:16:52 +0800

    clean up

--- calculate.js
+++ calculate.js
@@ -1,7 +1,6 @@
 const axios = require('axios')
 const fs = require('fs');
 
-
 module.exports = {
     bfx: async function() {
         const btcDataURL = "https://api-pub.bitfinex.com/v2/ticker/tBTCUSD"
@@ -10,7 +9,7 @@ module.exports = {
             //console.log(data[6])
 
         const satDenominator = 100000000
-        const hkdrate = 0.1287
+        const hkdrate = 0.1287 // approximate rate
         btcLastPrice = data[6]
         const sathkd = Math.round((1 / btcLastPrice) * satDenominator * hkdrate)
             //console.log("bitfinex last price: ", btcLastPrice, "current satHKD: ", sathkd)
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /**
  * Module dependencies.
- * source origin: https://github.com/expressjs/express/tree/master/examples/static-files
  */
 
 const express = require("express");
@@ -16,22 +15,6 @@ const zhcnjson = require('./locales/zh-cn.json');
 const zhhkjson = require('./locales/zh-hk.json');
 const enjson = require('./locales/en.json');
 
-
-// old default for testing
-let pydataold = {
-    'yeardata': [{ 'year': '1 year ago', 'sats': '1,199 ', 'percent': '-79.149' },
-        { 'year': '2 years ago', 'sats': '1,582 ', 'percent': '-84.197' },
-        { 'year': '3 years ago', 'sats': '1,958 ', 'percent': '-87.232' },
-        { 'year': '4 years ago', 'sats': '2,984 ', 'percent': '-91.622' },
-        { 'year': '5 years ago', 'sats': '21,122 ', 'percent': '-98.816' },
-        { 'year': '6 years ago', 'sats': '53,632 ', 'percent': '-99.534' },
-        { 'year': '7 years ago', 'sats': '40,367 ', 'percent': '-99.381' },
-        { 'year': '8 years ago', 'sats': '106,515 ', 'percent': '-99.765' },
-        { 'year': '9 years ago', 'sats': '1,016,962 ', 'percent': '-99.975' },
-        { 'year': '10 years ago', 'sats': '2,649,533 ', 'percent': '-99.991' }
-    ]
-}
-
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views')
 
@@ -41,36 +24,13 @@ app.engine('hbs', handlebars({
     extname: 'hbs',
 }));
 
-//Serves static files (we need it to import a css file)
 app.use(express.static('public'))
 
 // log requests
 app.use(logger('dev'));
-
-// express on its own has no notion
-// of a "file". The express.static()
-// middleware checks for a file matching
-// the `req.path` within the directory
-// that you pass it. In this case "GET /js/app.js"
-// will look for "./public/js/app.js".
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-// if you wanted to "prefix" you may use
-// the mounting feature of Connect, for example
-// "GET /static/js/app.js" instead of "GET /js/app.js".
-// The mount-path "/static" is simply removed before
-// passing control to the express.static() middleware,
-// thus it serves the file correctly by ignoring "/static"
 app.use('/static', express.static(path.join(__dirname, 'public')));
-
-// if for some reason you want to serve files from
-// several directories, you can use express.static()
-// multiple times! Here we're passing "./public/css",
-// this will allow "GET /style.css" instead of "GET /css/style.css":
 app.use(express.static(path.join(__dirname, 'public', 'css')));
-//app.use(express.json({ extended: false }));
 
 
 app.get('/', function(req, res) {
