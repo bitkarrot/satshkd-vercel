7b698fe72ddad95be57af231e8874577a4ebcf16
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-03-21 00:25:28 -0700

    add commands to install axios manually

--- test.js
+++ test.js
@@ -5,7 +5,15 @@ const axios = require('axios');
 // modify this to ping url, get data and update, commit and push file to github repo
 try {
 //  const res = poll.main()
-  console.log("main response: this is a test file trial ")
+  console.log("main response: this is a test file trial ");
+
+  const res = axios.get('https://google.com').then(response => {
+    return response;
+  }).catch(err => {
+    console.log(err);
+  });
+  
+  console.log(" this is the res: " , res);
   core.setOutput('✅ Success');
 
 } catch (error) {
