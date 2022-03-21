d0bf81bf21e040fdbf574bff183954051a1da457
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-03-20 22:48:18 -0700

    add node setup version

--- btcpoll.js
+++ btcpoll.js
@@ -8,58 +8,58 @@ const fileToWrite = dirPath + "/public/hkd_historical"
 const fileToRead = dirPath + "/public/hkd_historical"
 
 
-    // get btc/usd and btc/hkd daily rate
-    async function BTCDaily() {
-        let url = "https://api.coingecko.com/api/v3/coins/bitcoin/history?localization=false&date="
+// get btc/usd and btc/hkd daily rate
+async function BTCDaily() {
+    let url = "https://api.coingecko.com/api/v3/coins/bitcoin/history?localization=false&date="
 
-        const yesterday = moment().subtract(1, 'days') // YYYY-MM-DD
-        const reverse = yesterday.format('DD-MM-YYYY')
+    const yesterday = moment().subtract(1, 'days') // YYYY-MM-DD
+    const reverse = yesterday.format('DD-MM-YYYY')
 
-        // format is YYYY-MM-DD
-        const dbdate = yesterday.format('YYYY-MM-DD')
-        let full_url = url + reverse
-        let row = {}
-        //console.log("db date: ", dbdate)
-        //console.log("new date format:  ", reverse, "\n")
+    // format is YYYY-MM-DD
+    const dbdate = yesterday.format('YYYY-MM-DD')
+    let full_url = url + reverse
+    let row = {}
+    //console.log("db date: ", dbdate)
+    //console.log("new date format:  ", reverse, "\n")
 
-        await axios.get(full_url).then(
-            async function(response) {
-                // console.log("full url: ", full_url)
-                const data = await response.data;
-                const btcusd = data['market_data']['current_price']['usd']
-                const btchkd = data['market_data']['current_price']['hkd']
-                const satsrate = 100000000
-                const sathkd = parseInt(satsrate / btchkd)
-                const usdsat = parseInt(satsrate / btcusd)
+    await axios.get(full_url).then(
+        async function(response) {
+            // console.log("full url: ", full_url)
+            const data = await response.data;
+            const btcusd = data['market_data']['current_price']['usd']
+            const btchkd = data['market_data']['current_price']['hkd']
+            const satsrate = 100000000
+            const sathkd = parseInt(satsrate / btchkd)
+            const usdsat = parseInt(satsrate / btcusd)
 
-                row = {
-                    btcusd_rate: parseInt(btcusd),
-                    date: dbdate,
-                    usdsat_rate: usdsat,
-                    sathkd_rate: sathkd,
-                    btchkd_rate: parseFloat(btchkd).toFixed(2),
-                }
-                console.log("row data: ", row)
-            })
-        return row
+            row = {
+                btcusd_rate: parseInt(btcusd),
+                date: dbdate,
+                usdsat_rate: usdsat,
+                sathkd_rate: sathkd,
+                btchkd_rate: parseFloat(btchkd).toFixed(2),
+            }
+            console.log("row data: ", row)
+        })
+    return row
+}
+
+// update file in the target github repo
+async function updateFile() {
+    const row = await BTCDaily()
+
+    if (Object.keys(row).length > 0) {
+        //console.log("dirpath", dirPath)
+        //console.log("dirname", __dirname)
+        const original = await fs.readFileSync(fileToRead)
+        let orig = JSON.parse(original)
+        //console.log(orig[0])
+        orig.push(row)
+        //console.log(orig[orig.length - 1])
+        const new_content = JSON.stringify(orig)
+        await fs.writeFileSync(fileToWrite, new_content);
     }
-
-    // update file in the target github repo
-    async function updateFile() {
-        const row = await BTCDaily()
-
-        if (Object.keys(row).length > 0) {
-            //console.log("dirpath", dirPath)
-            //console.log("dirname", __dirname)
-            const original = await fs.readFileSync(fileToRead)
-            let orig = JSON.parse(original)
-            //console.log(orig[0])
-            orig.push(row)
-            //console.log(orig[orig.length - 1])
-            const new_content = JSON.stringify(orig)
-            await fs.writeFileSync(fileToWrite, new_content);
-        }
-    }
+}
 
 module.exports = {
     // start here
