03daee30ce70d8f6d0758a3e580a448a83888118
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-03-20 22:44:42 -0700

    try imports

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 const core = require('@actions/core');
+const fs = require('fs')
+const axios = require('axios');
+const moment = require('moment');
+
+const path = require('path');
+const dirPath = path.join(__dirname, ".");
+const fileToWrite = dirPath + "/public/hkd_historical"
+const fileToRead = dirPath + "/public/hkd_historical"
 
 // modify this to ping url, get data and update, commit and push file to github repo
 try {
